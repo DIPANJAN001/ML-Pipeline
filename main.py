0f1d383f52b9eea55d5fb916136e2cf687978046
Dipanjan <dipanjanpatra98@gmail.com>
2023-10-22 13:12:55 +0530

    Edited

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from ml_main_pipeline import MLMainPipeline
-import streamlit as st
+
 if __name__ == "__main__":
     main_pipeline = MLMainPipeline()
     accuracy, report = main_pipeline.run_pipeline()
-    st.write(f"Accuracy: {accuracy} %")
-    # print("Model Accuracy:", accuracy)
-    # print("Classification Report:\n", report)
+
+    print("Model Accuracy:", accuracy)
+    print("Classification Report:\n", report)
